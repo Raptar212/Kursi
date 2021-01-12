2e752571acaae59443c79c1e03ab9ee6825f13d8
Zlatous Leha <generalgrevus@gmail.com>
2021-01-12 19:44:21 +0200

    Initial commit

---
+++ src/First/Main.java
@@ -0,0 +1,13 @@
+package First;
+
+public class Main {
+
+    public static void main(String[] args){
+	    int x = 2, z = 1, c = z + x ;
+	    System.out.println("Sum: " + c);
+
+        System.out.println("Hello World! \nВсем привет!");
+
+
+    }
+}
---
+++ src/First/q.java
@@ -0,0 +1,12 @@
+package First;
+
+//import java.until.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+     System.out.println("Hello world");
+
+
+    }
+
+}
